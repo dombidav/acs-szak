c2604bbf17371a1cf044393fd4ba32bcd3291dab
Dombi Tibor <dombidav@gmail.com>
2020-11-30 15:17:12 +0100

    Configure serve for development

--- src/server/bootstrap/app.php
+++ src/server/bootstrap/app.php
@@ -23,9 +23,9 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -94,7 +94,10 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-
+$app->register(Laravel\Tinker\TinkerServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
+$app->configure('cors');
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
