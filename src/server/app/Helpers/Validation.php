bf5ebbaf9ca80a92ef2f490ce7c9c8bde3c9331c
Dombi Tibor <dombidav@gmail.com>
2020-12-01 18:10:42 +0100

    Added Validation Helper

---
+++ src/server/app/Helpers/Validation.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Helpers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class Validation
+{
+    /** @var array $rules */
+    private $rules;
+    /** @var Request $request */
+    private $request;
+
+    /**
+     * Validation constructor.
+     * @param array $rules
+     * @param Request $request
+     */
+    public function __construct(Request $request, array $rules)
+    {
+        $this->rules = $rules;
+        $this->request = $request;
+    }
+
+    public static function Define(Request $request, array $rules)
+    {
+        return new Validation($request, $rules);
+    }
+
+    public function Call(callable $call)
+    {
+        try {
+            Validator::make($this->request->all(), $this->rules);
+            $temp = $call();
+            return response()->json($temp, 201);
+        } catch (Exception $exception) {
+            $data['code'] = $exception->getCode();
+            $data['message'] = $exception->getMessage();
+            return response()->json($data, 400);
+        }
+    }
+}
