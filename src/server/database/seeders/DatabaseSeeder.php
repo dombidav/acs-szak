8f416a9eca708a85698db34e7b4bea16040b72a2
Dombi Tibor <dombidav@gmail.com>
2020-12-03 11:59:02 +0100

    Added Seeders to DatabaseSeeder

--- src/server/database/seeders/DatabaseSeeder.php
+++ src/server/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $this->call(PermissionSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
