4c7d5a870b9684592c5b63790356b027aeba9138
Dombi Tibor <dombidav@gmail.com>
2020-12-03 11:51:37 +0100

    Implemented ResourceModel for API Resources

---
+++ src/server/app/Models/ResourceModel.php
@@ -0,0 +1,109 @@
+<?php
+
+
+namespace App\Models;
+
+
+use App\Traits\CamelCasing;
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+/**
+ * Class ResourceModel Base model for Resources
+ * @package App\Models
+ * @method static ResourceModel findOrFail($id)
+ */
+abstract class ResourceModel extends Model
+{
+    use HasFactory, Uuid, CamelCasing;
+
+    public $incrementing = false;
+    protected $guarded = ['created_at', 'updated_at', 'uuid'];
+
+    //protected $primaryKey = 'uuid';
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d'
+    ];
+    protected $keyType = 'string';
+
+    public static function random()
+    {
+        return self::query()->inRandomOrder()->get()->first->first();
+    }
+
+    public function CallCreateSchema($schemaName)
+    {
+        $this->CreateSchema($schemaName);
+    }
+
+    public function CreateSchema($schemaName)
+    {
+        $definition = $this->GetDefinition();
+        Schema::create($schemaName, function (Blueprint $table) use ($definition) {
+            $table->string('id')->primary();
+
+            foreach ($definition as $key => $value) {
+                $a = null;
+                $rules = explode('|', $value);
+                switch ($rules[0]) {
+                    case 'int':
+                        $a = $table->integer($key);
+                        break;
+                    case 'bool':
+                        $a = $table->boolean($key);
+                        break;
+                    case 'float':
+                        $a = $table->float($key);
+                        break;
+                    case 'timestamp':
+                        $a = $table->timestamp($key);
+                        break;
+                    case 'string':
+                    default:
+                        $a = $table->string($key);
+                }
+                $nullable = true;
+                foreach ($rules as $rule) {
+                    if ($rule == 'required')
+                        $nullable = false;
+                    else if (Str::startsWith($rule, 'unique'))
+                        $a = $a->unique();
+                    else if (Str::startsWith($rule, 'default'))
+                        $a = $a->default(Str::after($rule, ':'));
+                }
+                if ($nullable)
+                    $a = $a->nullable();
+
+                $a->comment('');
+            }
+            $table->timestamps();
+        });
+    }
+
+    public function GetDefinition($for = null)
+    {
+        if (is_null($for))
+            $for = array_keys($this->definition());
+        else if (!is_array($for))
+            $for = [$for];
+
+        $result = [];
+
+        foreach ($this->definition() as $key => $value) {
+            if (in_array($key, $for))
+                $result[$key] = $value;
+        }
+        return $result;
+    }
+
+    /**
+     * Defines the model
+     * @return array
+     */
+    protected abstract function definition();
+}
